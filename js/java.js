05b6de315b694cc16121642faf75160a65dbe0d0
Mukudzei Chisenwa <e3740138@student.dcccd.edu>
2025-04-28 22:47:53 -0500

    java update

--- js/java.js
+++ js/java.js
@@ -1,8 +1,8 @@
+//Prompt the user for their name and display a welcome message
 var guest = prompt("Welcome! Please enter your name.");
 document.write("<h2>Welcome to my page, " + guest + "!</h2>")
 
-// This function changes the style of the page
-
+// This function changes the theme of the page
 function changeStyle(newStyle){
 	document.getElementById('color').setAttribute('href', newStyle);
 }
